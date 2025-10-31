0ef6063c5e8d03178a32e367226d7ff9885484c9
Shahd <shahdelmahdy22@gmail.com>
2025-11-01 00:51:08 +0200

    app edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 // src/App.js - Update your router
-import React from "react";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Cart from "./components/Cart/Cart";
+import Home from "./components/Home/Home.jsx";
+import Cart from "./components/Cart/Cart.jsx";
 
-import Layout from "./components/Layout/Layout";
+import Layout from "./components/Layout/Layout.jsx";
 
-import Checkout from "./components/Checkout/Checkout";
-import Products from "./Components/Products/Products";
-import { CartProvider } from "./Components/Context/CartContext";
-
+import Checkout from "./components/Checkout/Checkout.jsx";
+import Products from "./Components/Products/Products.jsx";
+import { CartProvider } from "./components/Context/CartContext.jsx";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout/>,
+      element: <Layout />,
       children: [
         { index: true, element: <Home /> },
         { path: "products", element: <Products /> },
         { path: "cart", element: <Cart /> },
-        { path: "checkout", element: <Checkout/> },
+        { path: "checkout", element: <Checkout /> },
       ],
     },
   ]);
